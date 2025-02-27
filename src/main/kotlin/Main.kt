713ed2526f6d3e67c42fcac9d08459b3dd4ec404
Andrei <sharov89@gmail.com>
2025-02-27 21:37:08 +0300

    2nd chapter extraz

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 package org.example
 
 const val HERO_NAME = "Madrigal"
+const val TAVERN_NAME = "Unicorn horn"
 
 fun main() {
     println("The hero announces her presence to the world.")
@@ -11,4 +12,11 @@ fun main() {
     println("The hero embarks on her journey to locate the enchanted sword.")
     playerLevel += 1
     println(playerLevel)
+
+    var hasSteed = false
+    val menu = mapOf("Mead" to 15, "Vine" to 20,  "Beer" to 10)
+    println("We have:")
+    println(menu.map { (drink, price) -> "$drink by price $price"})
+
+    println("A quick look in the mirror: " + HERO_NAME.reversed())
 }
